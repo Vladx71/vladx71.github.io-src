19826c89f3433efdd48e5f2d181369b68c6119f0
Vlad <vlad@vlad.hu>
2015-10-13 11:32:47 +0200

    plugins

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,7 +8,7 @@ SITEURL = ''
 
 PLUGIN_PATHS = ['/home/vlad/git/pelican-plugins/']
 PLUGINS = ['gravatar' , 'sitemap' , 'always_modified' , 'disqus_static' ,'encrypt-content' , 'gallery' , 'pdf' , 'pelican_youtube' , 'pelican_vimeo' , 'pin_to_top' , 'tag_cloud']
-SITEMAP['format'] = ['xml']
+SITEMAP = ['xml']
 
 PATH = 'content'
 
