6fde8411a073b4bd2960b87fce5ea1b17e58be3e
Vlad <vlad@vlad.hu>
2015-10-13 11:35:42 +0200

    plugins

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,11 +17,11 @@ TIMEZONE = 'Europe/Paris'
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = True 
-CATEGORY_FEED_ATOM = True
-TRANSLATION_FEED_ATOM = True
-AUTHOR_FEED_ATOM = True
-AUTHOR_FEED_RSS = True
+FEED_ALL_ATOM = None 
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
