0f8187d03ec8266409a2aca3c00777c2ae2a0a03
Vlad <vlad@vlad.hu>
2015-10-13 11:03:29 +0200

    HowTo

---
+++ Howto/publish.sh
@@ -0,0 +1,13 @@
+cd /home/vlad/git/ghpages/
+#make html && make serve
+make publish
+cd output
+git add .
+git commit -m "$1"
+git push -u origin master
+cd ..
+echo '*.pyc' >> .gitignore #don't need pyc files
+git add .
+git commit -m "$1"
+git push -u origin master
+
