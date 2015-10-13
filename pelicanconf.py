f10c2101134f3c445573aeb692518a69fbd73aca
Vlad <vlad@vlad.hu>
2015-10-13 11:29:59 +0200

    plugins

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,7 +6,7 @@ AUTHOR = u'Vladx71'
 SITENAME = u'Vladx71 Github Pages'
 SITEURL = ''
 
-PLUGIN_PATHS = ['/home/vlad/git/pelican-plugins']
+PLUGIN_PATHS = ['/home/vlad/git/pelican-plugins/']
 PLUGINS = ['gravatar' , 'sitemap' , 'always_modified' , 'disqus_static' ,'encrypt-content' , 'gallery' , 'pdf' , 'pelican_youtube' , 'pelican_vimeo' , 'pin_to_top' , 'tag_cloud']
 
 PATH = 'content'
